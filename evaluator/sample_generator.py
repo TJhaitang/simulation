daa731862a7a26ff71105fa0e943e9a227a2f5ca
TJhaitang <1319377413@qq.com>
2023-11-26 01:37:39 +0800

    1

--- evaluator/sample_generator.py
+++ evaluator/sample_generator.py
@@ -112,9 +112,9 @@ def t22_eval(n_features,s,n_packs,n_samples,h,L):
             pass
         elif i<L+1:
             #delta的前100项为独立同分布的高斯随机变量
-            delta[:100]=np.random.normal(0,h/100,100)
+            delta[:10]=np.random.normal(0,h/100,10)
         else:
-            delta[:100]=np.random.normal(0,2*s/100,100)
+            delta[:10]=np.random.normal(0,2*s/100,10)
         coef=coef_true+delta
         noise_mean = 0
         noise_var = 1
